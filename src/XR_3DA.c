b77a6729c9b265851b6224d2f9495a3eead11bd5
Alexander Popov <iiiypuk@iiiypuk.me>
2020-10-09 00:46:34 +0300

    add executable icon

--- src/XR_3DA.c
+++ src/XR_3DA.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <windows.h>
 
+#include "resource.h"
+
 int main()
 {
 	char CurrentPath[FILENAME_MAX];
@@ -9,6 +11,7 @@ int main()
 	SetCurrentDirectory("bin_x64");
 	_getcwd(CurrentPath, sizeof(CurrentPath));
 
+	// Run S.T.A.L.K.E.R.
 	system("xrEngine.exe");
 
 	return 0;
